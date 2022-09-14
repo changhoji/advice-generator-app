ad9717cae2cfc5e4a6d76087e994659dc0d2ee84
2021035487 <onkkno1@gmail.com>
2022-09-14 11:54:24 +0900

    create react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+
+function Main() {
+  return 
+}
+
+function App() {
+  return <div>
+    <Main></Main>
+  </div>
+  const [a, b] = useState(4);
+}
+
+export default App;
